cc307c017f6b7520d5ab436811dfe57958b0ae65
sameena <sameenasimmin323@gmail.com>
2024-09-18 20:13:47 +0530

    Added filtering ,pagination and search functionality for IPO's

--- IPO_WebApp/ipo/views.py
+++ IPO_WebApp/ipo/views.py
@@ -1,11 +1,16 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import IPO
 from .serializers import IPOSerializer
 from django.shortcuts import render
 
+
 class IPOViewSet(viewsets.ModelViewSet):
     queryset = IPO.objects.all()
     serializer_class = IPOSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    filterset_fields = ['status', 'price_band', 'listing_date']
+    search_fields = ['company__company_name', 'price_band']  # Allow searching by company name or price_band
 
 def home(request):
     return render(request, 'index.html')
